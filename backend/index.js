4e21672450ad171096a22bd837cf5971427cc80f
uranghishna <hishna.di@gmail.com>
2022-05-19 09:27:25 +0700

    config

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import db from './config/Database.js';
+import Users from './models/UserModel.js';
+
+const app = express();
+
+try{
+    await db.authenticate();
+    console.log('database connected');
+    await Users.sync();
+} catch(error){
+    console.log(error);
+}
+
+app.listen(5000, () => {
+  console.log('Server started on port 5000');
+});
